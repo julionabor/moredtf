1692b1849eda1f2811e8842303a57a5481e88419
Julio Nabor <julionabor@gmail.com>
2025-11-20 23:41:19 +0000

    fix: import use php mailer

--- sendmail.php
+++ sendmail.php
@@ -1,7 +1,6 @@
 <?php
-require_once __DIR__ . '/PHPMailer/src/Exception.php';
-require_once __DIR__ . '/PHPMailer/src/PHPMailer.php';
-require_once __DIR__ . '/PHPMailer/src/SMTP.php';
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 // Ativa erros para depuração temporária
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
